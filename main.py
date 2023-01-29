3872c65eb15ea07c393d04ab36e7619abc1d6b9a
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-29 18:44:52 -0300

    Criando adivinha

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+print("*********************************")
 print("Bem vindo ao jogo, vamos jogar ?!")
+print("*********************************")
+numero_chutado = 43
+
+chutando = input("Qual o valor do seu chute ? ")
+print("Você chutou: ", chutando)
+chute = int(chutando)
+if (chute == numero_chutado):
+    print("*********************************")
+    print("**** Você acertou!  PARABÉNS ****")
+    print("*********************************")
+else:
+    print("Você errou :(")
