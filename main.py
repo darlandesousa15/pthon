18bbaee476aae2fef59ec5a0831b70d5e0e04417
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-31 04:28:59 -0300

    Atualizando elif

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print("*********************************")
 numero_chutado = 43
 
 chutando = input("Qual o valor do seu chute ? ")
-#O input é a mesma coisa que o prompt
+# O input é a mesma coisa que o prompt
 print("Você chutou: ", chutando)
 
 chute   = int(chutando)
@@ -13,13 +13,14 @@ menor   = chute < numero_chutado
 acertou = chute == numero_chutado
 
 if (acertou):
+# Possui a opção de digitar os if e else elif sem usar os (), tanot que foi utilizado os dois modelos para relembrar essa informação
     print("*********************************")
     print("**** Você acertou!  PARABÉNS ****")
     print("*********************************")
 else:
-    if (maior):
+    if maior:
         print("Você errou! O seu chute é maior que o numero sorteado")
-    elif (menor):
+    elif menor:
         print("Você errou! O seu chute é menor que o numero sorteado")
 
 print("Jogo encerrado")
