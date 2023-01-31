ea8ffc021ad659769bacba347c5ff6179aba6bda
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-31 04:50:02 -0300

    Criando repetições

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ print("*********************************")
 print("Bem vindo ao jogo, vamos jogar ?!")
 print("*********************************")
 numero_chutado = 43
+tentativas = 3
 
-chutando = input("Qual o valor do seu chute ? ")
-# O input é a mesma coisa que o prompt
-print("Você chutou: ", chutando)
+while (tentativas > 0):
 
-chute   = int(chutando)
-maior   = chute > numero_chutado
-menor   = chute < numero_chutado
-acertou = chute == numero_chutado
+    chutando = input("Qual o valor do seu chute ? ")
+    # O input é a mesma coisa que o prompt
+    print("Número de tentativas: ", tentativas)
+    print("Você chutou: ", chutando)
 
-if (acertou):
-# Possui a opção de digitar os if e else elif sem usar os (), tanot que foi utilizado os dois modelos para relembrar essa informação
-    print("*********************************")
-    print("**** Você acertou!  PARABÉNS ****")
-    print("*********************************")
-else:
-    if maior:
-        print("Você errou! O seu chute é maior que o numero sorteado")
-    elif menor:
-        print("Você errou! O seu chute é menor que o numero sorteado")
+    chute   = int(chutando)
+    maior   = chute > numero_chutado
+    menor   = chute < numero_chutado
+    acertou = chute == numero_chutado
+
+    if (acertou):
+    # Possui a opção de digitar os if e else elif sem usar os (), tanot que foi utilizado os dois modelos para relembrar essa informação
+        print("*********************************")
+        print("**** Você acertou!  PARABÉNS ****")
+        print("*********************************")
+    else:
+        if maior:
+            print("Você errou! O seu chute é maior que o numero sorteado")
+        elif menor:
+            print("Você errou! O seu chute é menor que o numero sorteado")
+            print("*****************************************************")
+    tentativas = tentativas - 1
 
 print("Jogo encerrado")
