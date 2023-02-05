4822aeef2d6d60bc914711c8ee1609fd17ea1fc0
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-05 05:07:03 -0300

    Selecionador de jogos corrigido

--- jogos.py
+++ jogos.py
@@ -1,6 +1,5 @@
 import random
-import forca
-import main
+
 
 # o random.random é para gerar um numero aleatório
 # o random.randrange (x, y) é para gerar um numero aleatório do valor x ao y, mas o valor do y vai ser 1 a menos
@@ -14,6 +13,8 @@ print("(1) Forca (2) Adivinhação")
 jogo = int(input("Qual jogo ?"))
 
 if (jogo == 1):
-    print("Jogando jogo da forca")
+    import forca
 elif (jogo == 2):
-    print("Jogando adivinhação")
+    import main
+
+print("FIM!")
